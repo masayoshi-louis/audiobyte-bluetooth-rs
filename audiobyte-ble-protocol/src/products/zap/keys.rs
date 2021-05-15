38d7ac8657d2b38837d28c2776d6ca21067e9cf4
luyi <takamachi660@gmail.com>
2021-05-15 11:00:03 +0800

    more ZAP properties

--- audiobyte-ble-protocol/src/products/zap/keys.rs
+++ audiobyte-ble-protocol/src/products/zap/keys.rs
@@ -6,7 +6,7 @@ use crate::private::{add_cmd_key_mapping, CmdKeyMapping};
 
 use self::super::ZapPropertyKey;
 
-/// Zap Power on/off
+/// Zap power on/off
 pub struct Power;
 
 impl typemap::Key for Power {
@@ -45,7 +45,7 @@ impl crate::private::PropKeyInternal for DigitalVoltage {
 
 impl ZapPropertyKey for DigitalVoltage {}
 
-/// Zap Analog Voltage+
+/// Zap analog voltage+
 pub struct PositiveAnalogVoltage;
 
 impl typemap::Key for PositiveAnalogVoltage {
@@ -58,7 +58,7 @@ impl crate::private::PropKeyInternal for PositiveAnalogVoltage {
 
 impl ZapPropertyKey for PositiveAnalogVoltage {}
 
-/// Zap Analog Voltage-
+/// Zap analog voltage-
 pub struct NegativeAnalogVoltage;
 
 impl typemap::Key for NegativeAnalogVoltage {
@@ -71,15 +71,97 @@ impl crate::private::PropKeyInternal for NegativeAnalogVoltage {
 
 impl ZapPropertyKey for NegativeAnalogVoltage {}
 
+/// Zap digital capacitor SP(?)
+pub struct DigitalCapacitorSP;
+
+impl typemap::Key for DigitalCapacitorSP {
+    type Value = i32;
+}
+
+impl crate::private::PropKeyInternal for DigitalCapacitorSP {
+    const CMD_KEY: &'static str = "ZAPDCAPSP";
+}
+
+impl ZapPropertyKey for DigitalCapacitorSP {}
+
+/// Zap digital power on/off
+pub struct DigitalPower;
+
+impl typemap::Key for DigitalPower {
+    type Value = bool;
+}
+
+impl crate::private::PropKeyInternal for DigitalPower {
+    const CMD_KEY: &'static str = "ZAPD";
+}
+
+impl ZapPropertyKey for DigitalPower {}
+
+/// Zap analog power on/off
+pub struct AnalogPower;
+
+impl typemap::Key for AnalogPower {
+    type Value = bool;
+}
+
+impl crate::private::PropKeyInternal for AnalogPower {
+    const CMD_KEY: &'static str = "ZAPA";
+}
+
+impl ZapPropertyKey for AnalogPower {}
+
+/// Zap reference power on/off
+pub struct ReferencePower;
+
+impl typemap::Key for ReferencePower {
+    type Value = bool;
+}
+
+impl crate::private::PropKeyInternal for ReferencePower {
+    const CMD_KEY: &'static str = "ZAPR";
+}
+
+impl ZapPropertyKey for ReferencePower {}
+
+// TODO move
+// /// Vox connections
+// pub struct VoxConnection;
+//
+// impl typemap::Key for VoxConnection {
+//     type Value = bool;
+// }
+//
+// impl crate::private::PropKeyInternal for VoxConnection {
+//     const CMD_KEY: &'static str = "VOXCONN";
+// }
+//
+// impl ZapPropertyKey for VoxConnection {}
+//
+// /// Hub connections
+// pub struct HubConnection;
+//
+// impl typemap::Key for HubConnection {
+//     type Value = bool;
+// }
+//
+// impl crate::private::PropKeyInternal for HubConnection {
+//     const CMD_KEY: &'static str = "HUBCONN";
+// }
+
+impl ZapPropertyKey for HubConnection {}
+
 lazy_static! {
-pub(crate) static ref CMD_KEY_MAPPING: CmdKeyMapping = {
+pub(super) static ref CMD_KEY_MAPPING: CmdKeyMapping = {
     let mut hash_map = HashMap::new();
     add_cmd_key_mapping(&mut hash_map, Power);
     add_cmd_key_mapping(&mut hash_map, Params);
     add_cmd_key_mapping(&mut hash_map, DigitalVoltage);
     add_cmd_key_mapping(&mut hash_map, PositiveAnalogVoltage);
     add_cmd_key_mapping(&mut hash_map, NegativeAnalogVoltage);
+    add_cmd_key_mapping(&mut hash_map, DigitalCapacitorSP);
+    add_cmd_key_mapping(&mut hash_map, DigitalPower);
+    add_cmd_key_mapping(&mut hash_map, AnalogPower);
+    add_cmd_key_mapping(&mut hash_map, ReferencePower);
     hash_map
 };
 }
-
